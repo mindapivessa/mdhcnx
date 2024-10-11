22776b25b63042293afcc183779906a1ac72f050
Mind Apivessa <mindapi@Minds-MBP.fios-router.home>
2024-10-11 18:38:47 -0400

    update moodeng address

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const BASE_SEPOLIA_CHAIN_ID = 84532;
 export const usdcAddress = '0x036CbD53842c5426634e7929541eC2318f3dCF7e';
-export const mooDengAddress = '0x6e35d648c7a04071cB99BF6012423711B76F3C76';
+export const mooDengAddress = '0xA9Ba550Cc032Eb2365d4C8A82C5813DCc0E95cca';
 export const transferABI = [
   {
     "inputs":[{
